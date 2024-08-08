ab6c4d9648a81ba606015181cca8b5de09b2ca96
vignesh <vighneshsai17@gmail.com>
2024-08-08 11:58:21 +0530

    first commit

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ dotenv.config();
 app.use(express.json());
 
 app.use(cors({
-    origin: 'http://localhost:3000', // Allow requests from this origin
+   // Allow requests from this origin
+    origin: process.env.BASE_URI,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allow these HTTP methods
     credentials: true // Allow cookies to be sent
 }));
